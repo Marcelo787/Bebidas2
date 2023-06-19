4ab451a429341baf04930d77e35d9cb3522c977a
marcelo <marcelocoutinho777@hotmail.com>
2023-06-19 14:50:27 +0100

    Implementada o método onCreate do content provider

--- app/src/main/java/com/example/bebidas/BebidasContentProvider.kt
+++ app/src/main/java/com/example/bebidas/BebidasContentProvider.kt
@@ -6,6 +6,8 @@ import android.database.Cursor
 import android.net.Uri
 
 class BebidasContentProvider : ContentProvider() {
+    private var bdOpenHelper : BdBebidasOpenHelper? = null
+
     /**
      * Implement this to initialize your content provider on startup.
      * This method is called for all registered content providers on the
@@ -34,7 +36,8 @@ class BebidasContentProvider : ContentProvider() {
      * @return true if the provider was successfully loaded, false otherwise
      */
     override fun onCreate(): Boolean {
-        TODO("Not yet implemented")
+        bdOpenHelper = BdBebidasOpenHelper(context)
+        return true
     }
 
     /**
