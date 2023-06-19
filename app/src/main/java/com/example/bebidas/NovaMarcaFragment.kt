a966e6ae7ba6dca08bbfe8482df4bfa3955c3e80
marcelo <marcelocoutinho777@hotmail.com>
2023-06-19 22:50:55 +0100

    Adicionado fragmento para criar uma nova marca

---
+++ app/src/main/java/com/example/bebidas/NovaMarcaFragment.kt
@@ -0,0 +1,38 @@
+package com.example.bebidas
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.bebidas.databinding.FragmentNovaMarcaBinding
+
+class NovaMarcaFragment : Fragment() {
+    private var _binding: FragmentNovaMarcaBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentNovaMarcaBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val activity = activity as MainActivity
+        activity.fragment = this
+        activity.idMenuAtual = R.menu.menu_main
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
