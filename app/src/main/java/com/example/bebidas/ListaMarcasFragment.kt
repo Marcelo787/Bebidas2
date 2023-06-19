56f0a50091f16cc2d39a7beef1148513c61018e7
marcelo <marcelocoutinho777@hotmail.com>
2023-06-19 22:47:55 +0100

    Mostrar as opções alterar e eliminar apenas quando há marcas selecionadas

--- app/src/main/java/com/example/bebidas/ListaMarcasFragment.kt
+++ app/src/main/java/com/example/bebidas/ListaMarcasFragment.kt
@@ -29,7 +29,15 @@ class ListaMarcasFragment : Fragment(), LoaderManager.LoaderCallbacks<Cursor> {
     private val binding get() = _binding!!
 
     var marcaSelecionada : Marcas? = null
+        set(value) {
+            field = value
 
+            val mostrarEliminarAlterar = (value != null)
+
+            val activity = activity as MainActivity
+            activity.mostraOpcaoMenu(R.id.action_editar, mostrarEliminarAlterar)
+            activity.mostraOpcaoMenu(R.id.action_eliminar, mostrarEliminarAlterar)
+        }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
