7716ce09aa1b51a191c911ff48006c619e3bfad1
marcelo <marcelocoutinho777@hotmail.com>
2023-06-19 23:06:22 +0100

    Adicionada fragmento para eliminar marcas e correspondente menu

---
+++ app/src/main/java/com/example/bebidas/EliminarMarcaFragment.kt
@@ -0,0 +1,62 @@
+package com.example.bebidas
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.bebidas.databinding.FragmentEliminarMarcaBinding
+
+class EliminarMarcaFragment : Fragment() {
+    private var _binding: FragmentEliminarMarcaBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentEliminarMarcaBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val activity = activity as MainActivity
+        activity.fragment = this
+        activity.idMenuAtual = R.menu.menu_eliminar
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    fun processaOpcaoMenu(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_eliminar -> {
+                eliminar()
+                true
+            }
+            R.id.action_cancelar -> {
+                voltaLista()
+                true
+            }
+            else -> false
+        }
+    }
+
+    private fun voltaLista() {
+        findNavController().navigate(R.id.action_eliminarMarcaFragment_to_listaMarcasFragment)
+    }
+
+    private fun eliminar() {
+    }
+}
