448a84b74903693980514f57868633671956b48c
marcelo <marcelocoutinho777@hotmail.com>
2023-06-19 21:47:33 +0100

    Implementada função getItemCount do AdapterMarcas

--- app/src/main/java/com/example/bebidas/AdapterMarcas.kt
+++ app/src/main/java/com/example/bebidas/AdapterMarcas.kt
@@ -51,7 +51,7 @@ class AdapterMarcas : RecyclerView.Adapter<AdapterMarcas.ViewHolderMarca>() {
      * @return The total number of items in this adapter.
      */
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return cursor?.count ?: 0
     }
 
     /**
