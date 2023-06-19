095797ad2f2b94c28ce2f61e17959150e24d9c7a
marcelo <marcelocoutinho777@hotmail.com>
2023-06-19 19:10:28 +0100

    Processar as opções do menu da lista de bebidas no fragmento correspondente

--- app/src/main/java/com/example/bebidas/ListaBebidasFragment.kt
+++ app/src/main/java/com/example/bebidas/ListaBebidasFragment.kt
@@ -4,6 +4,7 @@ import android.database.Cursor
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.loader.app.LoaderManager
@@ -143,5 +144,35 @@ class ListaBebidasFragment : Fragment(), LoaderManager.LoaderCallbacks<Cursor> {
      */
     override fun onLoaderReset(loader: Loader<Cursor>) {
         adapterBebidas!!.cursor = null
+    }
+
+    fun processaOpcaoMenu(item: MenuItem) : Boolean {
+        return when (item.itemId) {
+            R.id.action_adicionar -> {
+                adicionaBebidas()
+                true
+            }
+            R.id.action_editar -> {
+                editarBebidas()
+                true
+            }
+            R.id.action_eliminar -> {
+                eliminarBebidas()
+                true
+            }
+            else -> false
+        }
+    }
+
+    private fun eliminarBebidas() {
+
+    }
+
+    private fun editarBebidas() {
+
+    }
+
+    private fun adicionaBebidas() {
+
     }
 }
