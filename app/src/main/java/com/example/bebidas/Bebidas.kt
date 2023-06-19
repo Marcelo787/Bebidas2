94c7a60799c59de7cb11a83880f585375475bd47
marcelo <marcelocoutinho777@hotmail.com>
2023-06-19 20:06:44 +0100

    Adicionado argumento (bebida) ao fragmento eliminar bebida

--- app/src/main/java/com/example/bebidas/Bebidas.kt
+++ app/src/main/java/com/example/bebidas/Bebidas.kt
@@ -3,14 +3,14 @@ package com.example.bebidas
 import android.content.ContentValues
 import android.database.Cursor
 import android.provider.BaseColumns
-
+import java.io.Serializable
 
 data class Bebidas(
     var nome: String,
     var descricao: String,
     var idMarca: Marcas,
     var id: Long = -1
-) {
+) : Serializable {
     fun toContentValues() : ContentValues {
         val valores = ContentValues()
 
